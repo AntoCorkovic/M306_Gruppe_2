c3a161dc822ac5bfe276968a39ccccefd1013015
QueSchoenenberger <schoenenbergerq@bzz.ch>
2024-05-31 16:39:39 +0200

    refactor structure

---
+++ src/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+from src.parser import Parser
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    parser = Parser()
+
+    #outflow_data = parser.parse_Outflow()
+    try:
+        inflow_data = parser.parse_Inflow()
+    except Exception as e:
+        print(e)
+    print(inflow_data)
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
