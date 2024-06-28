c6fab60c7b9086795f73218cb414b4e96d03cf49
janisoberholzer <oberholzerj@bzz.ch>
2024-06-28 14:20:28 +0200

    kll

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+import json
+from datetime import datetime, timedelta
 
 from flask import Flask, render_template
 
@@ -32,6 +33,37 @@ def ui():  # put application's code here
 
     return render_template('frontend/index.html')
 
+@app.route('/chart')
+def chart():
+    parser = Parser()
+
+    counterstands = parser.parse_counterstands()
+    consumptionvalues = parser.parse_consumptionvalues()
+    startdatetime = datetime(2019, 3, 20, 23, 0)
+    enddatetime = datetime(2019, 3, 21, 23, 30)
+
+    inflowObservations = parser.getObservationsForASpecificDuraction(startdatetime, enddatetime,
+                                                                     consumptionvalues.Inflows)
+    outflowObservations = parser.getObservationsForASpecificDuraction(startdatetime, enddatetime,
+                                                                      consumptionvalues.Outflows)
+    counterstandOfInflowAtStart = parser.getCounterStand(startdatetime, counterstands, consumptionvalues.Inflows)
+
+    # Extrahieren der Volumendaten und Zeitlabels
+    inflowData = [obs.Volume for obs in inflowObservations]
+    outflowData = [obs.Volume for obs in outflowObservations]
+    timeLabels = [(startdatetime + timedelta(minutes=15 * i)).strftime('%Y-%m-%d %H:%M') for i in
+                  range(len(inflowData))]
+
+    # Daten in einem Dictionary speichern
+    data = {
+        'inflowData': inflowData,
+        'outflowData': outflowData,
+        'timeLabels': timeLabels,
+        'counterstandOfInflowAtStart': counterstandOfInflowAtStart
+    }
+
+    # Daten direkt an das Template übergeben
+    return render_template('frontend/chart.html', data=json.dumps(data))
 
 if __name__ == '__main__':
     app.run()
